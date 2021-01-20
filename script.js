636d563672bdac4bfbab37069023a52b4bff0caa
Axel8555 <10234443+Axel8555@users.noreply.github.com>
2021-01-19 21:35:33 -0600

    Cambio para adaptarse a reglas gramaticales
    Si la palabra termina en consonante+m, intercambia sus posiciones a excepción de la p y b, pues en conjunto con la m son sonidos nasales.
    miguel -> mimgueml -> mimguelm
    Las palabras que usan los sonidos "qu" y "gu" ya no generan problemas: 
    quitar -> quimtarm
    Las palabras que tengan diptongos/hiatos de dos vocales ya no tienen problemas
    cheems -> cheems
    Aun así hay problemas con diptongos/hiatos de mas de dos vocales como "poema", y palabras con signos diacríticos en vocales.
    Otro problema encontrado es la palabra "como"
    como -> comom -> coom
    Tambien hay problemas para detectar cuando una palabra termina, pues solo se usa como criterio que haya un espacio, pero bien podria ser un punto o coma.
    miguel quiere verte -> mimguelm quiemrem vemrtem
    miguel, quiere verte -> mimgueml, quiemrem vemrtem

--- script.js
+++ script.js
@@ -1,7 +1,36 @@
 const parseText = (text) => {
-  let appendM = ['a', 'e', 'i', 'o', 'u']
-  let exclude = [[/my/g, 'y'], [/ml/g, 'l'], [/ma/g, 'a'], [/mu/g, 'u']]
-  let replace = [/n/g]
+  let appendM = "aáeéiíoóuú"//['a', 'e', 'i', 'o', 'u']
+  let exclude = 
+    [
+    //[/mb /g, 'bm '], sonido nasal
+    [/mc /g, 'dc '], 
+    [/md /g, 'dm '], 
+    [/mf /g, 'df '], 
+    [/mg /g, 'gm '], 
+    [/mh /g, 'hm '], 
+    [/mj /g, 'jm '], 
+    [/mk /g, 'km '], 
+    [/ml /g, 'lm '], 
+    [/md /g, 'dm '], 
+    //[/mp /g, 'pm '], sonido nasal
+    [/mq /g, 'qm '], 
+    [/mr /g, 'rm '], 
+    [/ms /g, 'sm '], 
+    [/mt /g, 'tm '], 
+    [/mv /g, 'vm '], 
+    [/mw /g, 'wm '], 
+    [/mx /g, 'xm '], 
+    [/my /g, 'ym '], 
+    [/mz /g, 'zm '],
+    [/guma/g, 'gua'], [/gume/g, 'gue'], [/gumi/g, 'gui'], [/gumo/g, 'guo'], [/gumu/g, 'guu'],
+    [/quma/g, 'qua'], [/qume/g, 'que'], [/qumi/g, 'qui'], [/qumo/g, 'quo'], [/qumu/g, 'quu'], 
+    [/amam/g, 'aam'], [/amem/g, 'aem'], [/amim/g, 'aim'], [/amom/g, 'aom'], [/amum/g, 'aum'],
+    [/emam/g, 'eam'], [/emem/g, 'eem'], [/emim/g, 'eim'], [/emom/g, 'eom'], [/emum/g, 'eum'],
+    [/imam/g, 'iam'], [/imem/g, 'iem'], [/imim/g, 'iim'], [/imom/g, 'iom'], [/imum/g, 'ium'],
+    [/omam/g, 'oam'], [/omem/g, 'oem'], [/omim/g, 'oim'], [/omom/g, 'oom'], [/omum/g, 'oum'], //comom -> coom error. liverpool -> limvemrpooml
+    //[/umam/g, 'uam'], [/umem/g, 'uem'], [/umim/g, 'uim'], [/umom/g, 'uom'], [/umum/g, 'uum']
+    ]
+  let replace = [] //= [/n/g]
   let textSplit = text.replace(/\\n/g, '\n').split('')
 
   let res = textSplit
